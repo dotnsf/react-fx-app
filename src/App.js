c56581f22edfb0cd71ded97567e2010177cd7e50
K.Kimura <dotnsf@gmail.com>
2022-01-02 22:37:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor( props ){
+    super( props );
+    this.state = {
+      datetime: null,
+      rate: null,
+      message: null
+    };
+
+    setInterval( () => {
+      fetch( 'https://dotnsf-fx.herokuapp.com/' ).then( res => res.json() ).then( data => {
+        console.log( data );
+        if( data && data.status ){
+          this.setState({
+            datetime: data.datetime,
+            rate: data.rate,
+            message: ''
+          });
+        }else{
+          this.setState({
+            datetime: null,
+            rate: null,
+            message: data.messsage
+          });
+        }
+      }).catch( e => {
+        this.setState({
+          datetime: null,
+          rate: null,
+          message: e
+        });
+      });
+    }, 10000 );
+  };
+
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.datetime}</p>
+        <div>
+          <ul>
+          {this.state.rate != null ? Object.keys( this.state.rate ).map( ( key, i ) => (
+            <li>{key} : {this.state.rate[key]}</li>
+          )) :
+            <li>{this.state.message}</li>
+          }
+          </ul>
+        </div>
       </header>
     </div>
-  );
+    );
+  };
 }
 
 export default App;
